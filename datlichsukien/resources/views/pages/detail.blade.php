9af6ddbc836b8db171b46d49db3bee526fe849c8
sylee <anhsypro123@gmail.com>
2019-11-05 23:13:52 +0800

    fix nam oc chó

--- datlichsukien/resources/views/pages/detail.blade.php
+++ datlichsukien/resources/views/pages/detail.blade.php
@@ -183,14 +183,26 @@ $photo_path = $data->unique('photo_path')->values();
           <label for="">Rating:</label>
           <?php session(['post_id' => $data[0]->id]); ?>
 
-          <span class="star-rating">
-            <input type="radio" name="rating" value="1">
-            <input type="radio" name="rating" value="2">
-            <input type="radio" name="rating" value="3">
-            <input type="radio" name="rating" value="4">
-            <input type="radio" name="rating" value="5">
-
-          </span>
+            <div class='rating-stars text-center'>
+              <ul id='stars'>
+                <li class='star' title='Poor' data-value='1'>
+                  <i class='fa fa-star fa-fw'></i>
+                </li>
+                <li class='star' title='Fair' data-value='2'>
+                  <i class='fa fa-star fa-fw'></i>
+                </li>
+                <li class='star' title='Good' data-value='3'>
+                  <i class='fa fa-star fa-fw'></i>
+                </li>
+                <li class='star' title='Excellent' data-value='4'>
+                  <i class='fa fa-star fa-fw'></i>
+                </li>
+                <li class='star' title='WOW!!!' data-value='5'>
+                  <i class='fa fa-star fa-fw'></i>
+                </li>
+              </ul>
+            </div>
+  
 
           <div class="form-group">
             <label for="">Comment:</label>
@@ -255,6 +267,6 @@ $photo_path = $data->unique('photo_path')->values();
   </div>
 
 </div>
-
-@endsection
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.3"></script>
+<script async defer crossorigin="anonymous" src="{{asset('js/front/cmt.js')}}"></script>
+@endsection
