ac5edcef161346dc956607b3f3704b778c90e741
namnk97 <khacnamxc97@gmail.com>
2019-11-06 23:58:46 +0700

    a

--- datlichsukien/resources/views/pages/detail.blade.php
+++ datlichsukien/resources/views/pages/detail.blade.php
@@ -183,25 +183,30 @@ $photo_path = $data->unique('photo_path')->values();
           <label for="">Rating:</label>
           <?php session(['post_id' => $data[0]->id]); ?>
 
-            <div class='rating-stars text-center'>
-              <ul id='stars'>
-                <li class='star' title='Poor' data-value='1'>
-                  <i class='fa fa-star fa-fw'></i>
+            <span class="star-rating">
+              <!-- <ul id='stars'>
+                <li class='star' name="rating" title='Poor' value='1'>
+                  <i class='fa fa-star fa-fw' name="rating"></i>
                 </li>
-                <li class='star' title='Fair' data-value='2'>
-                  <i class='fa fa-star fa-fw'></i>
+                <li class='star' title='Fair' name="rating" value='2'>
+                  <i class='fa fa-star fa-fw' name="rating"></i>
                 </li>
-                <li class='star' title='Good' data-value='3'>
-                  <i class='fa fa-star fa-fw'></i>
+                <li class='star' title='Good' name="rating" value='3'>
+                  <i class='fa fa-star fa-fw' name="rating"></i>
                 </li>
-                <li class='star' title='Excellent' data-value='4'>
-                  <i class='fa fa-star fa-fw'></i>
+                <li class='star' title='Excellent' name="rating" value='4'>
+                  <i class='fa fa-star fa-fw' name="rating"></i>
                 </li>
-                <li class='star' title='WOW!!!' data-value='5'>
-                  <i class='fa fa-star fa-fw'></i>
+                <li class='star' title='WOW!!!' name="rating" value='5'>
+                  <i class='fa fa-star fa-fw' name="rating"></i>
                 </li>
-              </ul>
-            </div>
+              </ul> -->
+              <input  class='fa fa-star fa-fw' type="radio" name="rating" value="1">
+              <input  class='fa fa-star fa-fw' type="radio" name="rating" value="2">
+              <input  class='fa fa-star fa-fw' type="radio" name="rating" value="3">
+              <input  class='fa fa-star fa-fw' type="radio" name="rating" value="4">
+              <input  class='fa fa-star fa-fw' type="radio" name="rating" value="5">
+            </span>
   
 
           <div class="form-group">
