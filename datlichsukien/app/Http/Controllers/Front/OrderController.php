48e9421a471f3f245e35b219db17c2c138e93dbc
tandd <khacnamxc97@gmail.com>
2019-11-06 17:54:22 +0700

    post

--- datlichsukien/app/Http/Controllers/Front/OrderController.php
+++ datlichsukien/app/Http/Controllers/Front/OrderController.php
@@ -17,36 +17,43 @@ class OrderController extends Controller
     public function index($id)
 	{
         $order_time=Order_time::all();
+        $restaurant=restaurant::find($id);
        
-  //       $post = DB::table('posts')
-  //       ->join('restaurants', 'posts.restaurant_id', '=', 'restaurants.id')
-		// ->select('restaurants.name')
-		// ->where('restaurants.id','=',$id);
-		return view('pages.order',['order_time'=>$order_time]);
+		return view('pages.order',['order_time'=>$order_time,'restaurant'=>$restaurant]);
 	}
 	public function addOrder(Request $request)
     {
-        // $this->validate($request,
-        //     [   'name'=>'required',
-        //         'address'=>'required',
+        $this->validate($request,
+            [   'price_table'=>'required',
+                'order_date'=>'required',
+                'people_number'=>'required',
+                'phone'=>'required',
+                'address'=>'required',
                 
-        //     ],
-        //     [
-        //         'name.required'=>'bạn chưa nhập tên địa điểm',
-        //         'address.required'=>'bạn chưa nhập địa chỉ',   
-        //     ]
-        // // );
-        $restaurant=Restaurant::all();
+            ],
+            [
+                'price_table.required'=>'bạn chưa nhập giá mỗi bàn ',
+                'order_date.required'=>'bạn chưa nhập ngày tổ chức',
+                'people_number.required'=>'bạn chưa nhập số lượng người',
+                'phone.required'=>'bạn chưa nhập tên địa điểm',
+                'address.required'=>'bạn chưa nhập địa chỉ',   
+            ]
+         );
+        // $restaurant = DB::table('restaurants')
+        // ->select('restaurants.id')
+        // ->where('restaurants.id','=',$id)->first()->id;
         $order = new order;
-
-        $order->number_people=$request->number_people;
+ 
+        $order->people_number=$request->people_number;
         $order->address=$request->address;
         $order->phone=$request->phone;
+        $order->user_id=Auth::id();
         $order->price_table=$request->price_table;  
-        $order->order_date=$request->date;
-        // $order->=$request->order_date; 
-        dd($request->phone);   
+        $order->order_date=$request->order_date;
+        $order->order_time_id=$request->order_time_id;
+        $order->restaurant_id=$request->restaurant_id; 
+         dd($request);   
         $order->save();
-        return view('' ,['restaurant'=>$restaurant]);
+        return view('pages.order',['order'=>$order]);
     }
 }
